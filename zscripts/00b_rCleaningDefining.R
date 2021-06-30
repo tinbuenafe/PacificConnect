74cf5b381a3e742f8d3ecc81f683585ab12fe65c
Isaac Brito-Morales <i.britomorales@uq.edu.au>
2021-06-30 11:27:58 +1000

    just minors

--- zscripts/00b_rCleaningDefining.R
+++ zscripts/00b_rCleaningDefining.R
@@ -9,6 +9,32 @@ source("zscripts/00a_fCleaningDefining.R")
 ####### 1. Running to get the correct input data for prioritizr
 ####################################################################################    
 # Reading the path where features are
+dirF <- list.files(path = "Output/FeaturesIUCNNew", pattern = ".rds", full.names = TRUE)
+fl <- vector("list", length = length(dirF))
+for(i in seq_along(fl)) {fl[[i]] <- iucn_mico(data = dirF[i], cost = "Output/Cost/costlayer.rds")}
+#  getting the cost
+out <- readRDS("Output/Cost/costlayer.rds") %>%
+  dplyr::select(cellsID, cost)
+# merge elements and adding ID+Cost
+final <- do.call(cbind, fl) %>%
+  as_tibble() %>%
+  mutate(cellsID = out$cellsID) %>%
+  left_join(out, "cellsID")
+# Converting the final element into a sf object and save it
+Fsf <- final %>%
+  st_as_sf(sf_column_name = "geometry")
+saveRDS(Fsf, "Prioritisation/InputsFeatures/01a_IUCN.rds")
+
+####################################################################################
+####### 2. Targets
+#################################################################################### 
+tIUCN <- trg_Mico("Prioritisation/InputsFeatures/02b_MiCONodes.rds")
+  saveRDS(tMiCO, "Prioritisation/InputsTargets/03b_TRGMiCONodes.rds")
+
+####################################################################################
+####### 1. Running to get the correct input data for prioritizr
+####################################################################################    
+# Reading the path where features are
   dirF <- list.files(path = "Output/FeaturesMiCONew", pattern = ".rds", full.names = TRUE)
   fl <- vector("list", length = length(dirF))
   for(i in seq_along(fl)) {fl[[i]] <- mico2(data = dirF[i], cost = "Output/Cost/costlayer.rds")}
@@ -31,5 +57,7 @@ source("zscripts/00a_fCleaningDefining.R")
   tMiCO <- trg_Mico("Prioritisation/InputsFeatures/02b_MiCONodes.rds")
     saveRDS(tMiCO, "Prioritisation/InputsTargets/03b_TRGMiCONodes.rds")
 
+    
+    
     
     
--- zscripts/00z_fPlottingCheck.R
+++ zscripts/00z_fPlottingCheck.R
@@ -19,14 +19,34 @@ library(stringr)
 library(data.table)
 
 source("zscripts/PacificConnect_HelpR.R")
-# MPAs sf
-mpas <- readRDS("Output/MPAs/PacificOcean_MPAs.rds")
-
 
 ft_Nodes <- readRDS("Prioritisation/InputsFeatures/02b_MiCONodes.rds")
 sps <- gather(data = ft_Nodes, "feature", "value", -c(cellsID, cost, geometry))
 features <- unique(sps$feature)
+for(i in seq_along(features)) {
+    sg <- sps %>% 
+      filter(feature == features[i])
+    st_crs(sg) <- "+proj=robin +lon_0=180 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+  # Color Palette, World borders and Legend
+    pal <- c("#deebf7", "#31a354")
+    type <- c("NO", "YES")
+  # Plotting
+    ggplot() +
+      geom_sf(data = sg, aes(group = as.factor(value), fill = as.factor(value)), color = NA) +
+      geom_sf(data = world_robinson, size = 0.05, fill = "grey20") +
+      coord_sf(xlim = c(st_bbox(sps)$xmin, st_bbox(sps)$xmax),
+               ylim = c(st_bbox(sps)$ymin, st_bbox(sps)$ymax),
+               expand = TRUE) +
+      theme_bw() +
+      scale_fill_manual(values = pal,
+                        name = "",
+                        labels = type) +
+      ggsave(paste0("Figures/FeaturesMiCO/", paste0(unique(sg$feature), ".png")), width = 15, height = 10, dpi = 300)
+}
 
+ft_IUCN <- readRDS("Prioritisation/InputsFeatures/01a_IUCN.rds")
+sps <- gather(data = ft_IUCN, "feature", "value", -c(cellsID, cost, geometry))
+features <- unique(sps$feature)
 for(i in seq_along(features)) {
   sg <- sps %>% 
     filter(feature == features[i])
@@ -45,5 +65,5 @@ for(i in seq_along(features)) {
     scale_fill_manual(values = pal,
                       name = "",
                       labels = type) +
-    ggsave(paste0("Figures/FeaturesMiCO/", paste0(unique(sg$feature), ".png")), width = 15, height = 10, dpi = 300)
+    ggsave(paste0("Figures/FeaturesIUCN/", paste0(unique(sg$feature), ".png")), width = 15, height = 10, dpi = 300)
 }
